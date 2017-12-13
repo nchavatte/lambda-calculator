8c23b292592d5b0828515d5b8288fa0e7f26b5f1
Nicolas CHAVATTE <nicolas.chavatte@edisys.eu>
2017-12-13 18:35:30 +0100

    Adds unit test

--- basicParsing/basicParser.spec.js
+++ basicParsing/basicParser.spec.js
@@ -3,10 +3,25 @@ var expect = require("chai").expect;
 
 describe("basicParser", function () {
     describe("parse", function () {
+
         it("returns parsing result", function () {
-            var script = "a=>b=>a";
+            var script = "a=>ab=>a";
             var actualResult = basicParser.parse(script);
             expect(actualResult).to.not.be.null;
+        });
+
+        it("returns right-structured lambda-term object", function(){
+            var script = "a=>ab=>a";
+            var actualResult = basicParser.parse(script);
+            expect(actualResult).to.deep.equal({
+                "type": "abstraction",
+                "argument": ["a"],
+                "body": {
+                    "type": "abstraction",
+                    "argument": ["a", "b"],
+                    "body": ["a"]
+                }
+            });
         });
     });
 });
