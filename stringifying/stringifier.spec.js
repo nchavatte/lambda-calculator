76b8e958d5f1eae9181a3559d22c558067b4bd3f
Nicolas CHAVATTE <nicolas.chavatte@edisys.eu>
2017-12-14 17:32:39 +0100

    Adds stringifier spec for application terms

--- stringifying/stringifier.spec.js
+++ stringifying/stringifier.spec.js
@@ -2,12 +2,28 @@ var stringify = require("./stringifier");
 var expect = require("chai").expect;
 
 describe("stringifier", function () {
-    it("returns right string on atomic term", function () {
+    it("returns right string for atomic term", function () {
         var atomicTerm = {
             type: "atomic",
             value: ["a", "b"]
         };
         var actualResult = stringify(atomicTerm);
         expect(actualResult).to.equal("ab");
+    });
+    
+    it("returns right string for application term", function () {
+        var applicationTerm = {
+            type: "application",
+            function: {
+                type: "atomic",
+                value: ["a"]
+            },
+            argument: {
+                type: "atomic",
+                value: ["b"]
+            }
+        };
+        var actualResult = stringify(applicationTerm);
+        expect(actualResult).to.equal("(a) b");
     });
 });
