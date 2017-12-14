1885d4796be4d061c2c147dfd3c3538275468bbf
Nicolas CHAVATTE <nicolas.chavatte@edisys.eu>
2017-12-14 17:55:19 +0100

    Implements stringifier for application terms

--- stringifying/stringifier.js
+++ stringifying/stringifier.js
@@ -1,11 +1,16 @@
 function stringify(term) {
     switch (term.type) {
+        case "application": return stringifyApplication(term);
         default: return stringifyAtomic(term);
     }
 }
 
+function stringifyApplication(applicationTerm) {
+    return "(" + stringify(applicationTerm.function) + ") " + stringify(applicationTerm.argument);
+}
+
 function stringifyAtomic(atomicTerm) {
-    return atomicTerm.value.reduce(function(acc, c){ return acc + c; }, "");
+    return atomicTerm.value.reduce(function (acc, c) { return acc + c; }, "");
 }
 
 module.exports = stringify;
