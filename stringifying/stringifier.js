760863e3ed77915f28076a174b029fed4445257d
Nicolas CHAVATTE <nicolas.chavatte@edisys.eu>
2017-12-14 18:11:50 +0100

    Implements stringifier for abstraction terms

--- stringifying/stringifier.js
+++ stringifying/stringifier.js
@@ -1,12 +1,18 @@
 function stringify(term) {
     switch (term.type) {
         case "application": return stringifyApplication(term);
+        case "abstraction": return stringifyAbstraction(term);
         default: return stringifyAtomic(term);
     }
 }
 
 function stringifyApplication(applicationTerm) {
     return "(" + stringify(applicationTerm.function) + ") " + stringify(applicationTerm.argument);
+}
+
+function stringifyAbstraction(abstractionTerm) {
+    var argumentString = abstractionTerm.argument.reduce(function (acc, c) { return acc + c; }, "");
+    return argumentString + " => " + stringify(abstractionTerm.body);
 }
 
 function stringifyAtomic(atomicTerm) {
