8c7dba3e3715602f9930a1bf811dff181fc8c07e
Nicolas CHAVATTE <nicolas.chavatte@edisys.eu>
2017-12-14 16:40:12 +0100

    Adds stringifier implementation for atomic terms

--- stringifying/stringifier.js
+++ stringifying/stringifier.js
@@ -1,1 +1,11 @@
-module.exports = function (term) { return null; };
+function stringify(term) {
+    switch (term.type) {
+        default: return stringifyAtomic(term);
+    }
+}
+
+function stringifyAtomic(atomicTerm) {
+    return atomicTerm.value.reduce(function(acc, c){ return acc + c; }, "");
+}
+
+module.exports = stringify;
